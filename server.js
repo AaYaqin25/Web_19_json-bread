1b4e10b40b668d4339537f2c06587bd7c1804af6
Ahmad Ainul Yaqin <bloodsakin@gmail.com>
2022-11-02 16:37:01 +0700

    Add Done

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const port = 3000
 
+const data = JSON.parse(fs.readFileSync('web.json', 'utf-8'));
+// data.forEach(element => {
+//   console.log(element.string);
+// });
+
+const app = express()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { inputData: data });
+})
+
+
+app.get('/add', (req, res) => {
+  res.render('add');
+})
+
+app.post('/add', (req, res) => {
+
+  data.push({
+    string: req.body.String,
+    integer: req.body.Integer,
+    float: req.body.Float,
+    date: req.body.Date,
+    boolean: req.body.Boolean
+  });
+  fs.writeFileSync("web.json", JSON.stringify(data, null, 3), "utf-8");
+  res.redirect("/");
+
+})
+
+app.get('/add', (req, res) => {
+  res.render('edit');
 })
 
 app.listen(port, () => {
-    console.log(`Aplikasi berjalan di port ${port}`);
+  console.log(`Aplikasi berjalan di port ${port}`);
 })
